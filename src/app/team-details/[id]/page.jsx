cffc40148000ad916839c426a62c7cc5da5ad044
hbashar434 <h.bashar434@gmail.com>
2025-07-19 16:12:59 +0600

    add instructor deatils page and instructor popular courses

--- src/app/team-details/[id]/page.jsx
+++ src/app/team-details/[id]/page.jsx
@@ -1,18 +1,26 @@
+"use client";
 import { Wrapper } from "@/layout";
 import TeamDetailsMain from "@/components/teams/team-details";
 import { instructors_data } from "@/data";
+import { useDetailsInstructorsInfo } from "@/data/instructors/use-instructors";
+import { useParams } from "next/navigation";
 
-export const metadata = {
-  title: 'Team Details Page'
-}
+const TeamDetailsPage = () => {
+  const { id } = useParams();
 
-const TeamDetailsPage = ({params}) => {
-    const team = instructors_data.find(item => Number(item.id) === Number(params.id))
-    return (
-        <Wrapper>
-            <TeamDetailsMain team={team} />
-        </Wrapper>
-    )
-}
+  const {
+    data: team = {},
+    isLoading,
+    isFetching,
+    error,
+    refetch,
+  } = useDetailsInstructorsInfo(id);
+
+  return (
+    <Wrapper>
+      <TeamDetailsMain team={team} />
+    </Wrapper>
+  );
+};
 
 export default TeamDetailsPage;
